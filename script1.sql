efe1ce0405ca5ce58857bb028a3e4964ed28cf2d
“Yuri <yura.kopshev@gmail.com>
2021-10-23 10:40:01 +0400

    Initial commit

---
+++ script1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CUSTOMERS
+(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    phone_number INT NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ script2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE ORDERS
+(
+    id INT AUTO_INCREMENT NOT NULL,
+    date DATE NOT NULL,
+    customer_id INT NOT NULL,
+    product_name VARCHAR(255),
+    amount INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id)
+);
